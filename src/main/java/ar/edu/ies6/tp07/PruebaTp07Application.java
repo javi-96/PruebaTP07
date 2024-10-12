f4d5795a77c44818df5d277ec93cf244ce47aeef
Javier Choque <alejandroj889@gmail.com>
2024-10-12 17:19:32 -0300

    Iniciando PruebaTP07 - Javier Choque

---
+++ src/main/java/ar/edu/ies6/tp07/PruebaTp07Application.java
@@ -0,0 +1,13 @@
+package ar.edu.ies6.tp07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTp07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTp07Application.class, args);
+	}
+
+}
