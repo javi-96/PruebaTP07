54fdb1a21b620ee0c75f4e6a2bd6f9479cc700a2
Javier Choque <alejandroj889@gmail.com>
2024-11-01 02:03:35 -0300

    Agregado html de materia - Javier Choque

--- src/main/java/ar/edu/ies6/tp07/controller/DocenteController.java
+++ src/main/java/ar/edu/ies6/tp07/controller/DocenteController.java
@@ -1,4 +1,5 @@
 package ar.edu.ies6.tp07.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,22 +8,21 @@ import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.ies6.tp07.model.Docente;
 import ar.edu.ies6.tp07.service.IDocenteService;
-import ar.edu.ies6.tp07.service.imp.DocenteServiceImp;
 
 @Controller
 public class DocenteController {
 
 //aqui lo primero que hace es instanciar por unica vez a docente
-@Autowired
-Docente unDocente;
+	@Autowired
+	Docente unDocente;
 
-@Autowired
-IDocenteService docenteService;
+	@Autowired
+	IDocenteService docenteService;
 
 //recibe laa petidicion docent
 
-@GetMapping("/docente") 
-public ModelAndView getIndexWithDocente () {
+	@GetMapping("/docente")
+	public ModelAndView getIndexWithDocente() {
 //Docente docente1 = new Docente (); //esto guarda un espacio en la memoria pero..
 //docente1.setApellido("Liquitay");
 //docente1.setNombre("Yanina");
@@ -31,30 +31,31 @@ public ModelAndView getIndexWithDocente () {
 //docente1.setFechaNacimiento(fechaNacimiento);
 //docente1.setEstado(null);
 //especifica la vista en el html
-ModelAndView transportador = new ModelAndView("docente");
-transportador.addObject("docente", unDocente);
+		ModelAndView transportador = new ModelAndView("docente");
+		transportador.addObject("docente", unDocente);
 
-return transportador;}
+		return transportador;
+	}
 
 //luego que se cargan los datos viene por post
-@PostMapping("/guardarDocente")
-public ModelAndView guardarDocente (Docente docente) {
-	//Este método devuelve un objeto ModelAndView, que combina la vista 
-	//(la página que se mostrará) y el modelo (los datos que se van a 
-	//mostrar en la vista).
-	
-	
-	
-	//aqui creamos el servi y mandamos a guardar perroo es un problema asi que creamos una
-	//inyeccion de dependencia
-	//DocenteServiceImp docenteService = new DocenteServiceImp() ;
-	docenteService.guardarDocente (docente);
-	
-	//aqui lista docente es la vista
-	//listadoDocente es la lista
-	ModelAndView transportador = new ModelAndView("listaDocentes");
-	transportador.addObject("listadoDocentes", docenteService.listarTodosDocente()); //estamos llamando al almacen de docentes
-	return transportador;
+	@PostMapping("/guardarDocente")
+	public ModelAndView guardarDocente(Docente docente) {
+		// Este método devuelve un objeto ModelAndView, que combina la vista
+		// (la página que se mostrará) y el modelo (los datos que se van a
+		// mostrar en la vista).
+
+		// aqui creamos el servi y mandamos a guardar perroo es un problema asi que
+		// creamos una
+		// inyeccion de dependencia
+		// DocenteServiceImp docenteService = new DocenteServiceImp() ;
+		docenteService.guardarDocente(docente);
+
+		// aqui lista docente es la vista
+		// listadoDocente es la lista
+		ModelAndView transportador = new ModelAndView("listaDocentes");
+		transportador.addObject("listadoDocentes", docenteService.listarTodosDocente()); // estamos llamando al almacen
+																							// de docentes
+		return transportador;
 
 	}
 
