2800f823fd3adc09e02eb3d2f3ee6500bea4ec0e
Estudiante <Estudiante@DESKTOP-J11OC32>
2024-11-01 02:14:12 -0300

    Agregado de @Entity a la clase alumno. Agregado al atributo Id @Id(jakarta para la importacion). Creacion del paquete Repository y una interface con el nombre AlumnoRepository. Agregue "extends" CrudRepository<Alumno String. Agregado de dependencias al pomx. Agregado de propiedades a application.properties. maizares judith marisel

--- src/main/java/ar/edu/ies6/tp07/controller/AlumnoController.java
+++ src/main/java/ar/edu/ies6/tp07/controller/AlumnoController.java
@@ -1,20 +1,24 @@
 package ar.edu.ies6.tp07.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.ies6.tp07.model.Alumno;
+import ar.edu.ies6.tp07.service.IAlumnoService;
 import ar.edu.ies6.tp07.service.imp.AlumnoServiceImp;
 
-
+@Controller
 public class AlumnoController {
 	
 	@Autowired
-	Alumno UnAlumno;
+	Alumno unAlumno;    //un alumno es un objeto de la clase alumno y alumno es una clase java
 	
-	
+	@Autowired
+	@Qualifier("servicioAlumnoBD")
 //gestiona el acceso a la vista
  //SpringBoot Springframework
 	
@@ -24,7 +28,7 @@ public ModelAndView getIndexWithAlumno () {
 	//codigo
 	//crea el alumno
 
-	Alumno unAlumno = new Alumno();
+	//Alumno unAlumno = new Alumno();
 	//Patron del diseño SINGLETON
 	//unAlumno.setNombre("judith");
     //unAlumno.setApellido("maizares");
@@ -44,11 +48,11 @@ public ModelAndView getIndexWithAlumno () {
 @PostMapping ("/guardarAlumno")
 public ModelAndView guardarAlumno (Alumno alumno) {
 	
-   AlumnoServiceImp  alumnoService = new AlumnoServiceImp ();
+ AlumnoServiceImp  alumnoService = new AlumnoServiceImp (); /////////////////////no me quiere tomar la interfaz de alumno////////////////////////////////
    alumnoService.guardarAlummno(alumno);
 	
    ModelAndView transportador = new ModelAndView ("listaAlumnos");
-   transportador.addObject("listadoAlumnos");                 //podria llamarlo directamente del almacen de alumnos pero treria problemas 
+   transportador.addObject("listadoAlumnos" ,alumnoService.listarTodosAlumnos());                 //podria llamarlo directamente del almacen de alumnos pero treria problemas 
 			                                                //porque no sabemos que tiene el almacen, nos traeria un problema de segurida..entonces le dejamos al service que haga ese trabajo
 			
 	return transportador;	
--- src/main/java/ar/edu/ies6/tp07/service/imp/AlumnoServiceImp.java
+++ src/main/java/ar/edu/ies6/tp07/service/imp/AlumnoServiceImp.java
@@ -2,20 +2,23 @@ package ar.edu.ies6.tp07.service.imp;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import ar.edu.ies6.tp07.model.Alumno;
+
 import ar.edu.ies6.tp07.service.IAlumnoService;
 import ar.edu.ies6.tp07.util.AlmacenAlumno;
 @Service
+@Qualifier("servicioAlumnoArrayList")
+
+
 public class AlumnoServiceImp implements IAlumnoService {
+	
+	
+	//AlmacenAlumno.alumno.add(alumno);
+	//System.out.println(AlmacenAlumno.alumno.get(0).getDni());
 
-	@Override
-	public void guardarAlummno(Alumno alumno){
-		// TODO Auto-generated method stub
-
-	AlmacenAlumno.alumno.add(alumno);
-		}
 		
 	
 	@Override
@@ -39,7 +42,13 @@ public class AlumnoServiceImp implements IAlumnoService {
 	@Override
 	public List<Alumno> listarTodosAlumnos() {
 		// TODO Auto-generated method stub
-		return null;
+		//filtrar por alumnos con estado activo
+		return AlmacenAlumno.alumno;
 	}
 
-}
+	@Override
+	public void guardarAlummno(Alumno alumno) {
+		// TODO Auto-generated method stub
+		
+	}
+	}
