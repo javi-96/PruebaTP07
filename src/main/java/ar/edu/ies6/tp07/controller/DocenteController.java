64afafcb7d7a6864cb63e3b9af566fe8129ed0de
YANINA <YANINA@AIWA_NA-1431>
2024-11-01 00:57:18 -0300

    porfin pude conectarme con la base de datos... Liquitay Elisa

--- src/main/java/ar/edu/ies6/tp07/controller/DocenteController.java
+++ src/main/java/ar/edu/ies6/tp07/controller/DocenteController.java
@@ -1,5 +1,6 @@
 package ar.edu.ies6.tp07.controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,10 +13,12 @@ import ar.edu.ies6.tp07.service.imp.DocenteServiceImp;
 @Controller
 public class DocenteController {
 
-//aqui lo primero que hace es instanciar por unica vez a docente
+//aqui lo primero que hace es instanciar por unica vez a docente.
+	//esto es una inyeccion de dependencia
 @Autowired
-Docente unDocente;
+Docente unDocente; //unDocente es un objeto de la clase docente. docente es una clase java
 
+@Qualifier ("servicioDocenteBD")
 @Autowired
 IDocenteService docenteService;
 
@@ -23,15 +26,8 @@ IDocenteService docenteService;
 
 @GetMapping("/docente") 
 public ModelAndView getIndexWithDocente () {
-//Docente docente1 = new Docente (); //esto guarda un espacio en la memoria pero..
-//docente1.setApellido("Liquitay");
-//docente1.setNombre("Yanina");
-//docente1.setDni("39202293");
-//LocalDate fechaNacimiento = LocalDate.of(2023, 4, 5); // 3 de abril de 2023
-//docente1.setFechaNacimiento(fechaNacimiento);
-//docente1.setEstado(null);
-//especifica la vista en el html
-ModelAndView transportador = new ModelAndView("docente");
+
+ModelAndView transportador = new ModelAndView("Docente");
 transportador.addObject("docente", unDocente);
 
 return transportador;}
@@ -53,7 +49,7 @@ public ModelAndView guardarDocente (Docente docente) {
 	//aqui lista docente es la vista
 	//listadoDocente es la lista
 	ModelAndView transportador = new ModelAndView("listaDocentes");
-	transportador.addObject("listadoDocentes", docenteService.listarTodosDocente()); //estamos llamando al almacen de docentes
+	transportador.addObject("listadoDocentes", docenteService.ListarTodosDocentes()); //estamos llamando al almacen de docentes
 	return transportador;
 
 	}
